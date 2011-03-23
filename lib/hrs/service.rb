6011a45edee0398be9615f311ecb7f6ae0df5131
Marco Metz <marco.metz@gmail.com>
2011-03-23 16:03:50 +0100

    hrs.available_hotels working

--- lib/hrs/service.rb
+++ lib/hrs/service.rb
@@ -4,7 +4,7 @@ require "savon"
 
 module HRS
   class Service < Rails::Engine
-    include ActionView::Helpers::TagHelper
+    #include ActionView::Helpers::TagHelper
   
     TestServer = "http://iut-service.hrs.com:8080"
     ProductionServer = "http://p-service.hrs.com:8080"
@@ -36,21 +36,22 @@ module HRS
     def request(procedure, data_to_send="")
       begin
         result = client.request procedure do |soap, wsdl|        
-          client_type = content_tag("clientType", Settings["ClientType"])
-          client_key = content_tag("clientKey", Settings["ClientKey"])
-          client_password = content_tag("clientPassword", Settings["ClientPassword"])
+          client_type = ct("clientType", Settings["ClientType"])
+          client_key = ct("clientKey", Settings["ClientKey"])
+          client_password = ct("clientPassword", Settings["ClientPassword"])
           
-          language = content_tag("language", content_tag("iso3Language", iso3language))
-          iso3Country = content_tag("iso3Country", Settings["Iso3Country"])
-          isoCurrency = content_tag("isoCurrency", Settings["IsoCurrency"])
+          language = ct("language", ct("iso3Language", iso3language))
+          iso3Country = ct("iso3Country", Settings["Iso3Country"])
+          isoCurrency = ct("isoCurrency", Settings["IsoCurrency"])
           
-          credentials = content_tag("credentials", client_type + client_key + client_password)
-          locale = content_tag("locale", language + iso3Country + isoCurrency)
+          credentials = ct("credentials", client_type + client_key + client_password)
+          locale = ct("locale", language + iso3Country + isoCurrency)
           
           procedure_request = "<#{procedure}Request xmlns=''>#{credentials}#{locale}#{data_to_send}</#{procedure}Request>"
           soap_xml = "<soap:Body><#{procedure} xmlns='com.hrs.soap.hrs'>#{procedure_request}</#{procedure}></soap:Body>"
           soap.xml = "<soap:Envelope xmlns:soap='http://schemas.xmlsoap.org/soap/envelope/'>#{soap_xml}</soap:Envelope>"
         end
+        result.to_hash
       rescue
         nil
       end
@@ -59,8 +60,8 @@ module HRS
     
     def ping
       begin
-        data = content_tag("echoData", "I am alive")
-        request("ping", data).to_hash[:ping_response][:ping_response]
+        data = ct("echoData", "I am alive")
+        request("ping", data)[:ping_response][:ping_response]
       rescue
         false
       end
@@ -70,32 +71,68 @@ module HRS
     def search_locations(city)
       begin
         data = "<fuzzySearch xmlns:xsi='...' xsi:nil='true' />"
-        data += content_tag("locationName", city)
-        data += content_tag("locationLanguage", content_tag("iso3Language", iso3language))
-        request("locationSearch", data).to_hash[:location_search_response][:location_search_response][:locations]
+        data += ct("locationName", city)
+        data += ct("locationLanguage", ct("iso3Language", iso3language))
+        request("locationSearch", data)[:location_search_response][:location_search_response][:locations]
       rescue
         {}
       end
     end
     
     #Default location_id for Berlin = 55133
-    def search_hotels(location_id)
+    def search_hotels(location_id, perimeter=1000, maxResults=0, minAverageRating=0, minCategory=0)
       begin
         locationCriterion = "<fuzzySearch xmlns:xsi='...' xsi:nil='true' />"
-        locationCriterion += content_tag("locationID", location_id)
-        locationCriterion += content_tag("perimeter", 1000)
+        locationCriterion += ct("locationId", location_id)
+        locationCriterion += ct("perimeter", perimeter)
         
-        searchCriterion = content_tag("locationCriterion", locationCriterion) 
-        searchCriterion += content_tag("hotelNames", "")
-        searchCriterion += content_tag("minCategory", "0")
-        searchCriterion += content_tag("minAverageRating", "0")
-        searchCriterion += content_tag("maxResults", "0")
-        data = content_tag("searchCriterion", searchCriterion )
+        searchCriterion = ct("locationCriterion", locationCriterion) 
+        searchCriterion += ct("hotelNames", "")
+        searchCriterion += ct("minCategory", minCategory)
+        searchCriterion += ct("minAverageRating", minAverageRating)
+        searchCriterion += ct("maxResults", maxResults)
+        data = ct("searchCriterion", searchCriterion )
         request("hotelSearch", data)
       rescue
         {}
       end
     end
+
+    #Default location_id for Berlin = 55133
+    def available_hotels(location_id, from_date, to_date, perimeter=5000, maxResults=0, minCategory=0, roomType="single", adultCount=1, orderKey="price", orderDirection="ascending" )
+      begin
+        locationCriterion = "<fuzzySearch xmlns:xsi='...' xsi:nil='true' />"
+        locationCriterion += ct("locationId", location_id)
+        locationCriterion += ct("perimeter", perimeter)
+        
+        searchCriterion = ct("locationCriterion", locationCriterion) 
+        searchCriterion += ct("minCategory", minCategory)
+        searchCriterion += ct("maxResults", maxResults)
+        
+        availCriterion = ct("from", from_date )
+        availCriterion += ct("to", to_date )
+        availCriterion += "<minPrice xmlns:xsi='...' xsi:nil='true' />"
+        availCriterion += "<maxPrice xmlns:xsi='...' xsi:nil='true' />"
+        availCriterion += ct("includeBreakfastPriceToDetermineCheapestOffer", "true")
+        availCriterion += ct("roomCriteria", ct("id", "1") + ct("roomType", roomType) + ct("adultCount", adultCount))
+        
+        orderCriterion = ct("orderKey", orderKey)
+        orderCriterion += ct("orderDirection", orderDirection)
+        
+        data = ct("searchCriterion", searchCriterion ) + ct("availCriterion", availCriterion ) + ct("orderCriterion", orderCriterion ) 
+        request("hotelAvail", data)[:hotel_avail_response][:hotel_avail_response][:hotel_avail_hotel_offers]
+      rescue
+        {}
+      end
+    end
+
+
+    private
+    
+    def ct(tag, text)
+      "<#{tag}>#{text}</#{tag}>"
+    end
+
     
   end
 end
